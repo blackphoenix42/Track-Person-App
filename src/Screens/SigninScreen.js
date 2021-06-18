c2fca1e419afe7b0faa0c68e53f7a21c9c8e5756
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 00:38:10 +0530

    Add Signin screen

--- src/Screens/SigninScreen.js
+++ src/Screens/SigninScreen.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { StyleSheet, View } from 'react-native'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+import { Context } from '../context/AuthContext'
+import { NavigationEvents } from 'react-navigation'
 
 const SigninScreen = () => {
+    const { state, signin, clearErrorMessage } = useContext(Context)
     return (
-        <View>
-            <Text>Signin Screen</Text>
+        <View style={styles.container}>
+            <NavigationEvents onWillBlur={clearErrorMessage} />
+            <AuthForm
+                headerText="Sign In to your Account"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign In"
+                onSubmit={signin}
+            />
+            <NavLink
+                text="Don't have an account? Sign up instead"
+                routeName="Signup"
+            />
         </View>
     )
 }
 
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    }
+}
+
 export default SigninScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+})
