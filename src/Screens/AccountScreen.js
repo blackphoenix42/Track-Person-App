96d8731f5ae84625c4473a405ea486fad2988811
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 00:51:26 +0530

    Add Acoount Screen

--- src/Screens/AccountScreen.js
+++ src/Screens/AccountScreen.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { StyleSheet, Text } from 'react-native'
+import { Button } from 'react-native-elements'
+import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
+import { SafeAreaView } from 'react-navigation'
 
 const AccountScreen = () => {
+    const { signout } = useContext(AuthContext)
     return (
-        <View>
-            <Text>Account Screen</Text>
-        </View>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text style={{ fontSize: 48 }}>Account Screen</Text>
+            <Spacer>
+                <Button title="Sign Out" onPress={signout} />
+            </Spacer>
+        </SafeAreaView>
     )
 }
 
