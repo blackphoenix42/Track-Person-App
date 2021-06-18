5c45295cf12a6bd4f6137577482a725b0b5161a0
Ayush Yadav <aayush.sang@gmail.com>
2021-06-18 21:01:58 +0530

    Add CORS

--- Server/src/index.js
+++ Server/src/index.js
@@ -7,11 +7,12 @@ const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
 const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(authRoutes)
 app.use(trackRoutes)
