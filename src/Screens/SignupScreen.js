34d679e3fefc3fea22cac18012b24410d1e80358
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 00:38:00 +0530

    Refactor Code

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -1,45 +1,26 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Text, Input, Button } from 'react-native-elements'
-import Spacer from '../components/Spacer'
 import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
+import { NavigationEvents } from 'react-navigation'
+
 
 const SignupScreen = ({ navigation }) => {
-    const { state, signup } = useContext(AuthContext)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
+    const { state, signup, clearErrorMessage } = useContext(AuthContext)
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <NavigationEvents onWillBlur={clearErrorMessage} />
+            <AuthForm
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer />
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <NavLink
+                routeName="Signin"
+                text="Already have an account? Sign in instead"
             />
-
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => signup({ email, password })}
-                />
-            </Spacer>
         </View>
     )
 }
@@ -57,11 +38,5 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 200
-    },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
     }
 })
---
+++ src/components/NavLink.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import Spacer from './Spacer'
+import { withNavigation } from 'react-navigation'
+
+const NavLink = ({ navigation, text, routeName }) => {
+    return (
+        <TouchableOpacity onPress={() => navigation.navigate(routeName)}>
+            <Spacer>
+                <Text style={styles.link}>{text}</Text>
+            </Spacer>
+        </TouchableOpacity>
+    )
+}
+export default withNavigation(NavLink)
+
+const styles = StyleSheet.create({
+    link: {
+        color: 'blue'
+    }
+})
