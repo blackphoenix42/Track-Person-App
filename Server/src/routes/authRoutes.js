0f8effcade527f09c1e26d2ea17a85f04e6e6941
Ayush Yadav <aayush.sang@gmail.com>
2021-06-18 13:57:43 +0530

    Add Routes

---
+++ Server/src/routes/authRoutes.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+const jwt = require('jsonwebtoken')
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = new User({ email, password })
+        await user.save()
+        const token = jwt.sign({ userId: user._id }, `${process.env.JWT}`)
+        res.send({ token })
+    } catch (err) {
+        res.status(422).send(err.message)
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must Provide email and password' })
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.status(422).send({ error: 'Email not Found' })
+    }
+
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({ userId: user._id }, `${process.env.JWT}`)
+        res.send({ token })
+    }
+    catch (err) {
+        res.status(422).send({ error: 'Invalid Password or email' })
+    }
+})
+
+module.exports = router
