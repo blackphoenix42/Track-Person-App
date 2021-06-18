091f1cc59c39c7df106d7969d5363726ae9268d0
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 00:37:11 +0530

    Add Resolve Auth Screen

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import SignupScreen from "./src/Screens/SignupScreen";
 import TrackCreateScreen from "./src/Screens/TrackCreateScreen";
 import TrackDetailScreen from "./src/Screens/TrackDetailScreen";
 import TrackListScreen from "./src/Screens/TrackListScreen";
+import ResolveAuthScreen from "./src/Screens/ResolveAuthScreen";
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Provider as AuthProvider } from './src/context/AuthContext'
+import { setNavigator } from "./src/navigationRef";
 
 const switchNavigator = createSwitchNavigator({
+  ResolveAuth: ResolveAuthScreen,
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen
@@ -30,7 +33,7 @@ const App = createAppContainer(switchNavigator)
 export default () => {
   return (
     <AuthProvider>
-      <App />
+      <App ref={(navigator) => setNavigator(navigator)} />
     </AuthProvider>
   )
 }
