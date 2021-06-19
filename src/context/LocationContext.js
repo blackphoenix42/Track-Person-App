b201b68277d03628b43aea0c28e4da0afe40baae
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 15:36:57 +0530

    Add Actions to start stop recording

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -3,24 +3,36 @@ import createDataContext from './createDataContext'
 const locationReducer = (state, action) => {
     switch (action.type) {
         case 'add_current_location': return { ...state, currentLocation: action.payload }
+        case 'add_location': return { ...state, locations: [...state.locations, action.payload] }
+        case 'start_recording': return { ...state, recording: true }
+        case 'stop_recording': return { ...state, recording: false }
+        case 'change_name': return { ...state, name: action.payload }
         default: return state
     }
 }
 
 const startRecording = dispatch => () => {
-
+    dispatch({ type: 'start_recording' })
 }
 
 const stopRecording = dispatch => () => {
+    dispatch({ type: 'stop_recording' })
 
 }
 
-const addLocation = dispatch => (location) => {
+const addLocation = dispatch => (location, recording) => {
     dispatch({ type: 'add_current_location', payload: location })
+    if (recording) {
+        dispatch({ type: 'add_location', payload: location })
+    }
+}
+
+const changeName = dispatch => (name) => {
+    dispatch({ type: 'change_name', payload: name })
 }
 
 export const { Provider, Context } = createDataContext(
     locationReducer,
-    { startRecording, stopRecording, addLocation },
-    { recording: false, locations: [], currentLocation: null }
+    { startRecording, stopRecording, addLocation, changeName },
+    { name: '', recording: false, locations: [], currentLocation: null }
 )
