55502b4ee96dd0cf6ac31f051fb9d6edf11a9a3f
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 16:09:27 +0530

    Add Authorization

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from 'axios'
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default axios.create({
-    baseURL: 'http://02a1fe3eb426.ngrok.io'
+
+const instance = axios.create({
+    baseURL: 'http://34695297ed58.ngrok.io',
+
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    }
+)
+
+export default instance
