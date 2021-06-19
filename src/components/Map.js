8616e0e5a9175f438aef42469fda4f4e919ca4aa
Ayush Yadav <aayush.sang@gmail.com>
2021-06-19 15:36:30 +0530

    Track User using Polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,12 +4,11 @@ import MapView, { Polyline, Circle } from 'react-native-maps'
 import { Context as LocationContext } from '../context/LocationContext'
 
 const Map = () => {
-    const { state: { currentLocation } } = useContext(LocationContext)
+    const { state: { currentLocation, locations } } = useContext(LocationContext)
 
     if (!currentLocation) {
         return <ActivityIndicator size="large" style={{ marginTop: 200 }} color="#0000ff" />
     }
-
     return (
         <MapView
             style={styles.map}
@@ -24,6 +23,10 @@ const Map = () => {
                 radius={30}
                 strokeColor="rgba(158,158,255,1.0)"
                 fillColor="rgba(158,158,255,0.3)"
+            />
+
+            <Polyline
+                coordinates={locations.map(loc => loc.coords)}
             />
         </MapView>
     )
